731fe21e79e2d4ffaf8f184d158e6c6b530e13af
ratnasai2206 <kothamasuvenkataratnasai@gmail.com>
2024-03-19 10:39:57 +0530

    Application properties added successfully

--- pet-consultation/src/main/java/com/onlinepetconsultation/dto/AdminDto.java
+++ pet-consultation/src/main/java/com/onlinepetconsultation/dto/AdminDto.java
@@ -1,11 +1,6 @@
 package com.onlinepetconsultation.dto;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
--- pet-consultation/src/main/java/com/onlinepetconsultation/repository/ConsultantRepository.java
+++ pet-consultation/src/main/java/com/onlinepetconsultation/repository/ConsultantRepository.java
@@ -1,9 +1,13 @@
 package com.onlinepetconsultation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.onlinepetconsultation.entity.Consultant;
+import com.onlinepetconsultation.util.ConsultantRoles;
 
 public interface ConsultantRepository extends JpaRepository<Consultant, Integer> {
 
+	public List<Consultant> findByDesignation(ConsultantRoles designation);
 }
